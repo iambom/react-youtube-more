bd9d92f118c6613df2515ad8372155dc70ad818e
보미니 <iambxmk@gmail.com>
2021-03-30 12:51:15 +0900

    convert published time with each time unit

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -17,6 +17,48 @@ const VideoDetail = ({video, video : {snippet}, channelLogo}) => {
         }
         return answer;
     }
+
+    const getPublishedTime = () => {
+        //현재시간
+        let now = new Date(); 
+        //게시 시간 
+        let writeDay = new Date(snippet.publishedAt);
+        // console.log(writeDay)
+        let answer = '';
+        let publishedTime = '';
+        if(now.getFullYear() > writeDay.getFullYear()) {
+            answer = now.getFullYear() - writeDay.getFullYear();
+            publishedTime = `${answer}년 전`
+        }else if(now.getMonth() > writeDay.getMonth()) {
+            answer = now.getMonth() - writeDay.getMonth();
+            publishedTime = `${answer}달 전`;
+        }else if(now.getDate() > writeDay.getDate()) {
+            answer = now.getDate() - writeDay.getDate();
+            publishedTime = `${answer}일 전`;
+        }else if(now.getDate() == writeDay.getDate()){
+            let nowTime = now.getTime();
+            let writeTime = writeDay.getTime();
+            if(nowTime > writeTime) {
+                let sec, min, hour, day;
+                sec = parseInt(nowTime - writeTime) / 1000;
+                day = parseInt(sec/60/60/24);
+                sec = (sec - (day * 60 * 60 * 24));
+                hour = parseInt(sec/60/60);
+                sec = (sec - (hour*60*60));
+                min = parseInt(sec/60);
+                sec = parseInt(sec-(min*60));
+                if(hour > 0) {
+                    publishedTime = `${hour}시간 전`;
+                }else if(min > 0) {
+                    publishedTime = `${min}분 전`;
+                }else if(sec > 0) {
+                    publishedTime = `${sec}초 전`;
+                }
+            }
+        }
+        return publishedTime;
+    }
+    
     return(
         <div className={styles.container}>
             <div className={styles.iframe_wrap}>
@@ -26,7 +68,7 @@ const VideoDetail = ({video, video : {snippet}, channelLogo}) => {
             <div className={styles.count_wrap}>
                 <div className={styles.count_left}>
                     <span>조회수 {getViewCount()}회</span>
-                    <span>{snippet.publishedAt}</span>
+                    <span>{getPublishedTime()}</span>
                 </div>
                 <div className={styles.count_right}>
                     <button className={styles.btn_like}>{video.statistics.likeCount}</button>
--- src/components/VideoItem/VideoItem.jsx
+++ src/components/VideoItem/VideoItem.jsx
@@ -18,6 +18,47 @@ const VideoItem = ({video, video : {snippet}, channelLogo, onVideoClick, display
         }
         return answer;
     }
+
+    const getPublishedTime = () => {
+        //현재시간
+        let now = new Date(); 
+        //게시 시간 
+        let writeDay = new Date(snippet.publishedAt);
+        // console.log(writeDay)
+        let answer = '';
+        let publishedTime = '';
+        if(now.getFullYear() > writeDay.getFullYear()) {
+            answer = now.getFullYear() - writeDay.getFullYear();
+            publishedTime = `${answer}년 전`
+        }else if(now.getMonth() > writeDay.getMonth()) {
+            answer = now.getMonth() - writeDay.getMonth();
+            publishedTime = `${answer}달 전`;
+        }else if(now.getDate() > writeDay.getDate()) {
+            answer = now.getDate() - writeDay.getDate();
+            publishedTime = `${answer}일 전`;
+        }else if(now.getDate() == writeDay.getDate()){
+            let nowTime = now.getTime();
+            let writeTime = writeDay.getTime();
+            if(nowTime > writeTime) {
+                let sec, min, hour, day;
+                sec = parseInt(nowTime - writeTime) / 1000;
+                day = parseInt(sec/60/60/24);
+                sec = (sec - (day * 60 * 60 * 24));
+                hour = parseInt(sec/60/60);
+                sec = (sec - (hour*60*60));
+                min = parseInt(sec/60);
+                sec = parseInt(sec-(min*60));
+                if(hour > 0) {
+                    publishedTime = `${hour}시간 전`;
+                }else if(min > 0) {
+                    publishedTime = `${min}분 전`;
+                }else if(sec > 0) {
+                    publishedTime = `${sec}초 전`;
+                }
+            }
+        }
+        return publishedTime;
+    }
     
     const displayType = display === 'list' ? styles.list : styles.grid;
     return (
@@ -31,7 +72,8 @@ const VideoItem = ({video, video : {snippet}, channelLogo, onVideoClick, display
                     <p className={styles.title}>{snippet.title}</p>
                     <p className={styles.channel_title}>{snippet.channelTitle}</p>
                     <span>조회수 {getViewCount()}회</span>
-                    <span>{snippet.publishedAt}</span>
+                    <span>{getPublishedTime()}</span>
+                    {/* <span>{snippet.publishedAt}</span> */}
                 </div>
             </div>
         </li>
