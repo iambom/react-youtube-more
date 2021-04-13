82c24fc8a917230d6c2a7fa8056027f307134901
보미니 <iambxmk@gmail.com>
2021-04-13 21:24:11 +0900

    fix css"

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -6,22 +6,24 @@ const VideoList = ({videos, channels, onVideoClick, display}) => {
     // console.log(videos)
     const displayType = display === 'grid' ? styles.grid : styles.list;
     return (
-        <ul className={`${styles.container} ${displayType}`}>
-            {
-                videos.map(video => {
-                    let channelLogo, channel;
-                    channels.forEach(element => {
-                        if(element.id === video.snippet.channelId) {
-                            channel = element;
-                            channelLogo = element.snippet.thumbnails.default.url;
-                        }
-                    });
-                    return (
-                        <VideoItem key={video.id} video={video} channel={channel} channelLogo={channelLogo} onVideoClick={onVideoClick} display={display}/>
-                    )
-                })
-            }
-        </ul>
+        <div className={styles.container}>
+            <ul className={`${displayType}`}>
+                {
+                    videos.map(video => {
+                        let channelLogo, channel;
+                        channels.forEach(element => {
+                            if(element.id === video.snippet.channelId) {
+                                channel = element;
+                                channelLogo = element.snippet.thumbnails.default.url;
+                            }
+                        });
+                        return (
+                            <VideoItem key={video.id} video={video} channel={channel} channelLogo={channelLogo} onVideoClick={onVideoClick} display={display}/>
+                        )
+                    })
+                }
+            </ul>
+        </div>
     )
 }
 
